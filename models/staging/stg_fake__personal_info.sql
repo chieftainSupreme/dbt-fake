75f4706da853ce7980ccb3195db88cfc01e30d66
Ethan Elias <ethanelias77@gmail.com>
2025-06-25 23:42:13 +0000

    materalize dim_people as table, feature engineering on stg_personal_info

--- models/staging/stg_fake__personal_info.sql
+++ models/staging/stg_fake__personal_info.sql
@@ -1,5 +1,11 @@
 with source as (
-    select *
+    select id
+      , CAST(REGEXP_SUBSTR(phone_number, '\\+(\\d+)', 1, 1, 'e', 1) AS INT) AS area_code
+      , CAST(REGEXP_SUBSTR(phone_number, '-\\s*(\\d+)', 1, 1, 'e', 1) AS INT) AS phone_number
+      , birthdate
+      , blood_type
+      , favorite_color
+      , credit_score
     from {{ source('raw_fake_company', 'fake_personal_info') }}
 )
 select *
