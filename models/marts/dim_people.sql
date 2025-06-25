599d6edc5f23772236967851dbf1f2caa374fcd9
Ethan Elias <ethanelias77@gmail.com>
2025-06-25 23:28:06 +0000

    build dim_people

---
+++ models/marts/dim_people.sql
@@ -0,0 +1,15 @@
+with people as (
+
+    select *
+    from {{ ref('stg_fake__people') }}
+
+)
+, personal_info as (
+
+    select *
+    from {{ ref('stg_fake__personal_info') }}
+
+)
+select *
+from people p
+join personal_info i on p.id = i.id 
